b0f4ccc3b8c2ab906ec0b927b37eb1a98672835e
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-11-24 17:31:32 +0100

    Specify loader return type based on input

--- src/loader.ts
+++ src/loader.ts
@@ -1,6 +1,13 @@
 import { fabric, forge, LoaderId, quilt, vanilla } from "./";
 
-export function loader(id: LoaderId) {
+type Loader<Id extends LoaderId> = 
+    Id extends "vanilla" ? typeof vanilla
+  : Id extends "fabric" ? typeof fabric
+  : Id extends "quilt" ? typeof quilt
+  : Id extends "forge" ? typeof forge
+  : never;
+
+export function loader(id: LoaderId): Loader<typeof id> {
   if (id === "fabric") return fabric;
   if (id === "quilt") return quilt;
   if (id === "forge") return forge;
