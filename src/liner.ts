20e9f732962c3d1b10ac66b80ea292919862a738
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2024-03-13 20:19:21 +0100

    Remove comment

--- src/liner.ts
+++ src/liner.ts
@@ -1,15 +1,15 @@
 type LineConsumer = (line: string) => void;
 
 export function liner(lineConsumer: LineConsumer) {
-  let buffer = ''; // TODO Is there a StringBuilder in js?
+  let buffer = '';
   return (data: string) => {
     buffer += data;
 
     const lines = buffer.split('\n');
     buffer = lines.pop() ?? '';
 
-    for(let i = 0; i < lines.length; i++) {
+    for (let i = 0; i < lines.length; i++) {
       lineConsumer(lines[i]);
     }
-  }
+  };
 }
